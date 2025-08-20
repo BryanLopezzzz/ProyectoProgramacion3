7c32a098dcfaa175e65b2f76a365489fba9408be
BryanLopezzzz <bryan.lopez.juarez@est.una.ac.cr>
2025-08-20 14:08:18 -0600

    Intento primer FXML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module hospital {
+module ProyectoProgramacion3 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.xml;
-    requires java.desktop;
 
-    opens hospital to javafx.fxml;
+    opens hospital.controller to javafx.fxml;
     exports hospital;
+    exports hospital.controller;
+    exports hospital.view;
 }
