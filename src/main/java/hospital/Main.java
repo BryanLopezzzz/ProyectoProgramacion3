489939cb9377b2d73e505661b2fb66c97613216c
BryanLopezzzz <bryan.lopez.juarez@est.una.ac.cr>
2025-08-23 21:51:15 -0600

    Más fxml de Login

--- src/main/java/hospital/Main.java
+++ src/main/java/hospital/Main.java
@@ -1,4 +1,22 @@
 package hospital;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hospital/view/login-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Login Hospital");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module ProyectoProgramacion3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
+    opens hospital.view to javafx.fxml;
     opens hospital.controller to javafx.fxml;
     exports hospital;
     exports hospital.controller;
